9953f7d3d368bbbdc4ee89dcc4df929b19cfe1c6
Piyush Raj <rpiyush1357@gmail.com>
2025-08-06 13:29:58 +0530

    decorators with agruments

--- decorated.py
+++ decorated.py
@@ -1,12 +1,27 @@
-def calculate(decorated):
-    def inner():
-        print("Hey this line is before the function gets decorated")
-        decorated()
-        print("Hey this line is after the function gets decorated")
+# def calculate(decorated):
+#     def inner():
+#         print("Hey this line is before the function gets decorated")
+#         decorated()
+#         print("Hey this line is after the function gets decorated")
+#     return inner
+
+# @calculate
+# def add():
+#     print("I'm executed from the decorated function")
+
+# add()
+
+# Decorators with Arguments and Parameters
+
+def calculate(func):
+    def inner(a, b):
+        print(f"Going to perform division on, {a} and {b}")
+        func(a, b)
     return inner
 
 @calculate
-def add():
-    print("I'm executed from the decorated function")
+def divide(a, b):
+    print(a/b)
 
-add()
+divide(10, 5)
+
