1f5f08af9e65e478f25d01f5cf95b0aa9e2367e2
Piyush Raj <rpiyush1357@gmail.com>
2025-08-06 13:51:14 +0530

    Decorator chaining

--- decorated.py
+++ decorated.py
@@ -11,17 +11,43 @@
 
 # add()
 
-# Decorators with Arguments and Parameters
 
-def calculate(func):
-    def inner(a, b):
-        print(f"Going to perform division on, {a} and {b}")
-        func(a, b)
+# Decorators with Arguments and Parameters
+# def calculate(func):
+#     def inner(a, b):
+#         print(f"Going to perform division on, {a} and {b}")
+#         func(a, b)
+#     return inner
+
+# @calculate
+# def divide(a, b):
+#     print(a/b)
+
+# divide(10, 5)
+# divide(20, 5)
+
+
+# Decorator Chaining 
+def star(func):
+    def inner(*agrs, **kwagrs):
+        print("*" * 10)
+        func(*agrs, **kwagrs)
+        print("*" * 10)
     return inner
 
-@calculate
-def divide(a, b):
-    print(a/b)
+def percentage(func):
+    def inner(*agrs, **kwagrs):
+        print("%" * 10)
+        func(*agrs, **kwagrs)
+        print("%" * 10)
+    return inner
 
-divide(10, 5)
+@star
+@percentage
+def greet(msg):
+    print(msg)
+greet("hello")
+
+#this will be something like star(percentage(greet))
+# greet = star(percentage(greet))
 
